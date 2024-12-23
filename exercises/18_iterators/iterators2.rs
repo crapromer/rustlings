211ceb504612d3d5aac8deb48d4f32a68336bc16
crapromer <tianniehao@hotmail.com>
2024-12-23 13:27:23 +0800

    init

---
+++ exercises/18_iterators/iterators2.rs
@@ -0,0 +1,70 @@
+// In this exercise, you'll learn some of the unique advantages that iterators
+// can offer.
+
+// TODO: Complete the `capitalize_first` function.
+// "hello" -> "Hello"
+fn capitalize_first(input: &str) -> String {
+    let mut chars = input.chars();
+    let mut s = String::new();
+    match chars.next() {
+        None => s,
+        Some(first) => {s.push_str(&first.to_uppercase().to_string());s.push_str(&input[1..]);s},
+    }
+}
+
+// TODO: Apply the `capitalize_first` function to a slice of string slices.
+// Return a vector of strings.
+// ["hello", "world"] -> ["Hello", "World"]
+fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+    let mut res:Vec<String> = Vec::new();
+    for item in words{
+        let s = capitalize_first(item);
+        res.push(s);
+    }
+    res
+    // ???
+}
+
+// TODO: Apply the `capitalize_first` function again to a slice of string
+// slices. Return a single string.
+// ["hello", " ", "world"] -> "Hello World"
+fn capitalize_words_string(words: &[&str]) -> String {
+    // ???
+    let mut res:String = String::new();
+    for item in words{
+        let s = capitalize_first(item);
+        res.push_str(&s);
+    }
+    res
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        assert_eq!(capitalize_first("hello"), "Hello");
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(capitalize_first(""), "");
+    }
+
+    #[test]
+    fn test_iterate_string_vec() {
+        let words = vec!["hello", "world"];
+        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+    }
+
+    #[test]
+    fn test_iterate_into_string() {
+        let words = vec!["hello", " ", "world"];
+        assert_eq!(capitalize_words_string(&words), "Hello World");
+    }
+}
